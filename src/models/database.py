f61b9eca7da6c5f06dd2a2b73c91ba25a0e9d61c
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-10-20 18:12:38 +0200

    Use postgres if necessary
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- src/models/database.py
+++ src/models/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///db.sqlite3"
+if os.getenv("DATABASE_NAME") == "sqlite":
+    DATABASE_URL = "sqlite:///db.sqlite3"
+else:
+    DATABASE_URL = f"postgresql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
